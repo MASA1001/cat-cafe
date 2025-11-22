ea4f47ec7c8d29d1a95bb37af1bdff3d6c01bdf4
Masafumi Okamoto <masa4432@gmail.com>
2025-11-22 12:55:37 +0900

    first commit

---
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Http\Requests\Admin\UpdateBlogRequest;
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\Cat;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+class AdminBlogController extends Controller
+{
+    // ブログ一覧画面
+    public function index()
+    // {
+    //     return view('admin.blogs.index');
+    // }
+    {
+        // DBからブログ一覧を取得
+        //$blogs = Blog::all();
+        //$blogs = Blog::latest('updated_at')->limit(10)->get();
+        //$user = Auth::user();
+        //$user =  \Illuminate\Support\Facades\Auth::user();
+        $blogs = Blog::latest('updated_at')->simplePaginate(10);
+        // ビューに変数を渡す
+        return view('admin.blogs.index', ['blogs' => $blogs]);
+    }
+
+    // ブログ投稿画面
+    public function create()
+    {
+        return view('admin.blogs.create');
+    }
+
+    // ブログ投稿処理
+    public function store()
+    {
+        $validated = $request->validated();
+        $validated['iamge'] = $request->file('image')->store('blogs','public');
+        Blog::create($validated);
+        // $savedImagePath = $request->file('image')->store('blogs','public');
+        // $blog = new Blog($request->validated());
+        // $blog->image = $savedImagePath;
+        // $blog->save();
+
+        return to_route('admin.blogs.index')->with('success','ブログを投稿しました');
+    }
+    // 指定したIDのブログ編集画面
+    public function edit(Blog $blog)
+    {
+        //$blog = Blog::find($id);
+        //$blog = Blog::findOrFail($id);
+        //dd($blog);
+        //return view('admin.blogs.edit', compact('blog'));
+        //$user = Auth::user();
+        $categories = Category::all();
+        $cats = Cat::all();
+        return view('admin.blogs.edit',[
+//            'user' => $user,
+             'blog' => $blog,
+             'categories' => $categories,
+             'cats'=> $cats
+        ]);
+    }
+
+    //　指摘したIDのブログ更新処理
+    public function update(UpdateBlogRequest $request, string $id)
+    {
+        $blog = Blog::findOrFail($id);
+        $updateData = $request->validated();
+
+        // 画像を変更する場合
+        if ($request->has('image')) {
+            Storage::disk('public')->delete($blog->image);
+            // 変更後の画像をアップロード、保存パスを更新対象データにセット
+            $updateData['image'] = $request->file('image')->store('blogs', 'public');
+        }
+        $blog->category()->associate($updateData['category_id']);
+        $blog->update($updateData);
+        //$blog->cats()->attach($updateData['cats']);
+        $blog->cats()->sync($updateData['cats'] ?? []);
+
+        return to_route('admin.blogs.index')->with('success','ブログを更新しました');
+    }
+
+    // 指定したIDのブログの削除処理
+    public function destroy(string $id)
+    {
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+        Storage::disk('public')->delete($blog->image);
+
+        return to_route('admin.blogs.index')->with('success','ブログを削除しました');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminBlogController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AuthController;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+//Route:: view('/','index');
+
+// お問い合わせフォーム
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'sendMail']);
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
+
+//　管理画面
+Route::prefix('/admin')
+    ->name('admin.')
+    ->group(function (){
+        //ログイン時のみアクセス可能なルート
+        Route::middleware('auth')->group(function (){
+
+//ブログ
+Route::resource('/blogs', AdminBlogController::class)->except(['show']);
+// Route::get('/blogs', [AdminBlogController::class, 'index'])->name('blogs.index');
+// Route::get('/blogs/create', [AdminBlogController::class, 'create'])->name('blogs.create');
+// Route::post('/blogs', [AdminBlogController::class, 'store'])->name('blogs.store');
+// Route::get('/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('blogs.edit');
+// Route::put('/blogs/{blog}', [AdminBlogController::class, 'update'])->name('blogs.update');
+// Route::delete('/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('blogs.destroy');
+
+//　ユーザ管理
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+// ログアウト
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    });
+    Route::middleware('guest')->group(function (){
+        //　認証
+        Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+        });
+// Route::get('/admin/blogs', [App\Http\Controllers\Admin\AdminBlogController::class, 'index'])->name('admin.blogs.index');
+// Route::get('/admin/blogs/create', [App\Http\Controllers\Admin\AdminBlogController::class, 'create'])->name('admin.blogs.create');
+// Route::post('/admin/blogs', [App\Http\Controllers\Admin\AdminBlogController::class, 'store'])->name('admin.blogs.store');
+// //Route::post('/admin/blogs', [App\Http\Controllers\Admin\AdminBlogController::class, 'store'])->name('admin.blogs.store');
+// //Route::get('/admin/blogs/{blog}', [App\Http\Controllers\Admin\AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+// //Route::post('/admin/blogs/{blog}', [App\Http\Controllers\Admin\AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+// Route::get('/admin/blogs/{blog}/edit', [App\Http\Controllers\Admin\AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+// Route::put('/admin/blogs/{blog}', [App\Http\Controllers\Admin\AdminBlogController::class, 'update'])->name('admin.blogs.update');
+// Route::delete('/admin/blogs/{blog}', [App\Http\Controllers\Admin\AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+
